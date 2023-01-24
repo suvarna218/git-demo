6c783ce92ccd8856688aefb91e20cca1d6e56a68
suvarna218 <bg.suvarna@gmail.com>
2023-01-24 10:57:49 -0800

    added demo project to local repository

---
+++ src/main/java/com/nerchuko/gitdemo/App.java
@@ -0,0 +1,16 @@
+package com.nerchuko.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public void addData(String data) {
+    	System.out.println("hello "+data);
+    }
+}
