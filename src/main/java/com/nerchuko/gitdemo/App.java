a605978d72fc23a27ab6855c1f8e2f7dd9b9bb11
suvarna218 <98008576+suvarna218@users.noreply.github.com>
2023-01-24 23:24:58 -0800

    Update App.java
    new method added by dev1

--- src/main/java/com/nerchuko/gitdemo/App.java
+++ src/main/java/com/nerchuko/gitdemo/App.java
@@ -10,6 +10,9 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    public void addedMethod(){
+       System.out.println("new method added");
+    }
     public void addData(String data) {
     	if(data.equals(Constant.DATA_TYPE)) {
     		System.out.println("welcome to nerchuko batch");
